b3028e2a9e5e4609071f77949f16254fe8267e96
Henrique Cicero <henrique.c.cicero@hotmail.com>
2022-08-30 21:08:26 -0300

    inserir a liberação de cors e captura da requisição json

--- api/database.php
+++ api/database.php
@@ -1,5 +1,8 @@
 <?php
-
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+header('Access-Control-Allow-Headers: token, Content-Type, X-Requested-With');
+$_POST = json_decode(file_get_contents("php://input"),true);
 
 function query(string $sql): array {
     $dbType = 'mysql';
